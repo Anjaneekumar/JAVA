d52c8c5722b03881f14bb09605420b87b262029c
Anjaneekumar <anjaneekumarsingh01@gmail.com>
2022-11-01 21:19:42 +0530

    print even number in ASC & DESC order

--- 100Program/SumOfFirstnEvenNum.java
+++ 100Program/SumOfFirstnEvenNum.java
@@ -15,9 +15,22 @@ public class SumOfFirstnEvenNum {
         
     }
     public static void printNumber(int num){
+        int total=0;
+        int start=2;
+        System.out.println("\n The series ascending order: ");
+        while (start<=num) {
+            System.out.print(start+" ");
+            
+            start+=2;
+        }
+        System.out.println("\n The first n even numbers in descending are: ");
         while (num>1) {
             System.out.print(num+" ");
+            total += num;
             num-=2;
         }
+
+        System.out.println("\n The total is: "+total);
     }
+
 }
