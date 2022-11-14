e732b46baf030198caf5555ab393e9bf55ebbc18
Anjaneekumar Singh <anjaneekumarsingh01@gmail.com>
2022-11-14 08:59:13 +0530

    test

--- StringMethods.java
+++ StringMethods.java
@@ -1,6 +1,8 @@
 public class StringMethod{
     public static void main(String args[]){
         /// just test string practice questions
+        //program to tell weather a website ends with .com , .net , .org
         
+        System.out.println("The string will end with: .org");
     }
 }
